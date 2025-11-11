5b1014f45c47c935971ebfc3e731fd2f80ce7dc4
Itamar Mizrahi <theitamarmizrahi@gmail.com>
2025-11-11 20:24:10 +0200

    Add test comment at the end of server.py

--- server.py
+++ server.py
@@ -8,3 +8,6 @@ Handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("Server started at localhost:" + str(PORT))
     httpd.serve_forever()
+
+
+#test
